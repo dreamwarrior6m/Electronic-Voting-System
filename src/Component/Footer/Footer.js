19d4d4009cd29e03b0f1b632b311de5ae449612c
MD. Jahangir Alam <zahangirkhan506@gmail.com>
2024-01-28 11:35:23 +0600

    footer design change

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -5,37 +5,70 @@ import { FaFacebookF, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900  pt-10 pb-8 border-t-[1px] border-white/10">
-      <div className="max-w-7xl mx-auto">
-        <section className="flex flex-col text-center">
-          <h1 className="text-white font-bold text-4xl mb-1">DVS</h1>
-          <p className="text-2xl font-medium text-white">Digital Voting System</p>
-        </section>
-        <h1 className="block h-[1.2px] w-8/12 mx-auto rounded-full bg-white/70 my-5"></h1>
-        <section className="flex flex-col gap-3 ">
-          <div className="flex justify-center gap-3">
-            <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
-              <FaFacebookF></FaFacebookF>
-            </button>
-            <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800 transition">
-              <BsInstagram></BsInstagram>
-            </button>
-            <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
-              <BsTwitter></BsTwitter>
-            </button>
-            <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
-              <FaLinkedin></FaLinkedin>
-            </button>
-            <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
-              <BsYoutube></BsYoutube>
-            </button>
-          </div>
-          <div className="text-center text-white">
-            <p>© 2023 Automotive. All Rights Reserved.</p>
-          </div>
-        </section>
-      </div>
-    </footer>
+    <>
+      <footer className="p-10 bg-gray-900 border-t-[1px] border-white/10 text-white">
+        <div className="footer grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-7xl mx-auto justify-items-center opacity-95">
+          <nav>
+            <div className="max-w-7xl mx-auto">
+              <section className="flex flex-col text-center">
+                <h1 className="text-white font-bold text-4xl mb-1">DVS</h1>
+                <p className="text-2xl font-medium text-white">
+                  Digital Voting System
+                </p>
+              </section>
+              <h1 className="block h-[1.2px] w-8/12 mx-auto rounded-full bg-white/70 my-5"></h1>
+              <section className="flex flex-col gap-3 ">
+                <div className="flex justify-center gap-3">
+                  <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
+                    <FaFacebookF></FaFacebookF>
+                  </button>
+                  <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800 transition">
+                    <BsInstagram></BsInstagram>
+                  </button>
+                  <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
+                    <BsTwitter></BsTwitter>
+                  </button>
+                  <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
+                    <FaLinkedin></FaLinkedin>
+                  </button>
+                  <button className="text-gray-800 bg-white rounded-full p-2 hover:text-white hover:bg-gray-800  transition">
+                    <BsYoutube></BsYoutube>
+                  </button>
+                </div>
+              </section>
+            </div>
+          </nav>
+          <nav>
+            <header className="footer-title">Services</header>
+            <a className="link link-hover">Blood Donation</a>
+            <a className="link link-hover">Health Check</a>
+            <a className="link link-hover">Blood Bank</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Legal</header>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+          <nav>
+            <header className="footer-title">Explore</header>
+            <a className="link link-hover">Features</a>
+            <a className="link link-hover">Blood Group</a>
+            <a className="link link-hover">Security</a>
+            <a className="link link-hover">fund</a>
+          </nav>
+        </div>
+      </footer>
+      <footer className="px-10 py-8 bg-gray-900 text-white">
+        <div className="footer footer-center  max-w-5xl mx-auto">
+          <aside>
+            <p>
+              Copyright © 2023 - All right reserved by Save Life Blood Donation
+            </p>
+          </aside>
+        </div>
+      </footer>
+    </>
   );
 };
 
