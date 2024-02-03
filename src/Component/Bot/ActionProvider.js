9928199235cc30eb32586e3f911721a8a8fe7459
mahsin <mahsin1912@gmail.com>
2024-02-03 22:42:20 +0600

    Chatbot Added

---
+++ src/Component/Bot/ActionProvider.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  
+  const handleHello = () => {
+    const botMessage = createChatBotMessage('Hello! Nice to meet you.');
+
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
+
+  const handleHi = () => {
+    const botMessage = createChatBotMessage('Hi! Nice to meet you.');
+
+    setState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, botMessage],
+    }));
+  };
+
+
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            handleHello,
+            handleHi,
+          },
+        });
+      })}
+    </div>
+  );
+};
+
+export default ActionProvider;
