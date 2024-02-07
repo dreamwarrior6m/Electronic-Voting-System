5c0f496c47f64f9daed4c68028163cac40bc33b7
Anamul9901 <anamulhaque9901@gmail.com>
2024-02-07 20:22:19 +0600

    own election added by anamul

--- src/app/dashboard/allElections/page.js
+++ src/app/dashboard/allElections/page.js
@@ -3,6 +3,11 @@ import ShowElections from "@/Component/Dashboard/Admin/ShowElections";
 const page = () => {
   return (
     <div>
+      <div>
+          <h3 className="text-3xl font-bold text-gray-700 text-center pt-2 pb-6">
+            All Elections
+          </h3>
+        </div>
       <ShowElections />
     </div>
   );
--- src/app/dashboard/ownElections/page.js
+++ src/app/dashboard/ownElections/page.js
@@ -1,10 +1,16 @@
 import ShowElections from "@/Component/Dashboard/Admin/ShowElections";
+import OwnElection from "@/Component/Dashboard/OwnElection/OwnElection";
 
 const page = () => {
   return (
     <>
       <div>
-        <ShowElections />
+      <div>
+          <h3 className="text-3xl font-bold text-gray-700 text-center pt-2 pb-6">
+            My Elections
+          </h3>
+        </div>
+        <OwnElection />
       </div>
     </>
   );
