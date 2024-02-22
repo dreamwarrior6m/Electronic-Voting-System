5de3faf00e764c1a3fdab33bb2dc58bd4a5e39b6
mahsin <mahsin1912@gmail.com>
2024-02-23 00:09:48 +0600

    chatbot responsive for all device

--- src/app/(withLayout)/page.js
+++ src/app/(withLayout)/page.js
@@ -8,7 +8,7 @@ import Statistics from "@/Component/HomeComponents/Statistics/Statistics";
 export default function Home() {
   return (
     <div className="">
-      <div className="fixed bottom-0 right-0 pr-2 pb-6 md:pr-6 md:pb-6 z-10 max-w-7xl">
+      <div className="fixed bottom-0 right-0 pr-3 pb-3 md:pr-6 md:pb-6 z-10 max-w-7xl">
         <Toggle />
       </div>
       <div>
