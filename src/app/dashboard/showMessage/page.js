65086bd7d15570e663b511ce156d7fb121233160
mahsin <mahsin1912@gmail.com>
2024-03-07 12:37:18 +0600

    Feedbacks page responsive

--- src/app/dashboard/showMessage/page.js
+++ src/app/dashboard/showMessage/page.js
@@ -9,9 +9,12 @@ const ShowFeedback = () => {
   const { data: candidates = [], refetch } = useQuery({
     queryKey: ["candidates45"],
     queryFn: async () => {
-      const res = await axios.get("https://evs-delta.vercel.app/admin-feedback",{
-        withCredentials: true,
-      });
+      const res = await axios.get(
+        "https://evs-delta.vercel.app/admin-feedback",
+        {
+          withCredentials: true,
+        }
+      );
       return res.data;
     },
   });
@@ -26,7 +29,7 @@ const ShowFeedback = () => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Yes, Deleted it!",
-    }).then( async(result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
         const res = await axios.delete(
           `https://evs-delta.vercel.app/admin-feedback/${id}`
@@ -48,58 +51,45 @@ const ShowFeedback = () => {
       <div>
         <div className="mt-5 text-indigo-200/100">
           <div className="overflow-x-auto">
-            <table className="table font-semibold">
-              {/* head */}
-              <thead>
-                <tr className="text-center text-md font-semibold  border-b-2 border-gray-600">
-                  <th>
-                    <label>
-                      <p className="">No</p>
-                    </label>
-                  </th>
-                  <th className=" ">Name</th>
-                  <th className="">Email</th>
-                  <th className="">Massage</th>
-                  <th className="">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {/* map candidates to rows */}
-                {candidates.map((candidate, index) => (
-                  <tr
-                    key={candidate.id}
-                    className={`${
-                      index % 2 === 1 ? "bg-blue-200/5 text-indigo-200/70" : "bg-blue-200/5 text-indigo-200/70"
-                    } text-center font-semibold border-b border-indigo-200/50`}
-                  >
-                    <th>
-                      <label>
-                        <p className=" font-semibold text-center">
-                          {index + 1}
-                        </p>
-                      </label>
-                    </th>
-                    <td className="">
-                      <div className="flex items-center justify-center gap-3">
-                        <div className="avatar"></div>
-                        <div>
-                          <div className="font-bold">
-                            {candidate.userName}
-                          </div>
-                        </div>
-                      </div>
-                    </td>
-                    <td>{candidate.email}</td>
-                    <td>{candidate.feedback}</td>
-                    <td className="text-3xl cursor-pointer">
-                      <button onClick={() => handledeleted(candidate._id)}>
-                        <MdDeleteForever className=" text-red-700" />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {/* head */}
+            <div className="hidden lg:block">
+              <div
+                className="grid grid-cols-12 text-center text-indigo-200/100
+                  font-semibold text-base border-b-2 border-indigo-200/50 py-3"
+              >
+                <p className=" col-span-1">No</p>
+                <p className=" col-span-3">Name</p>
+                <p className="col-span-3">Email</p>
+                <p className="col-span-4">Massage</p>
+                <p className="col-span-1">Action</p>
+              </div>
+            </div>
+
+            <div>
+              {/* map candidates to rows */}
+              {candidates.map((candidate, index) => (
+                <div
+                  key={candidate.id}
+                  className={`${
+                    index % 2 === 0
+                      ? "bg-blue-200/5 text-indigo-200/70"
+                      : "bg-blue-200/5 text-indigo-200/70"
+                  } text-center font-semibold border-b border-indigo-200/50 space-y-1 lg:grid grid-cols-12 py-3 justify-center items-center`}
+                >
+                  <p className=" font-semibold text-center col-span-1">
+                    {index + 1}
+                  </p>
+                  <p className="font-bold col-span-3">{candidate.userName}</p>
+                  <p className="col-span-3">{candidate.email}</p>
+                  <p className="col-span-4">{candidate.feedback}</p>
+                  <div className="text-3xl cursor-pointer col-span-1">
+                    <button onClick={() => handledeleted(candidate._id)}>
+                      <MdDeleteForever className=" text-red-700" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -108,5 +98,3 @@ const ShowFeedback = () => {
 };
 
 export default ShowFeedback;
-
-
